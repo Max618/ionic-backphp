3e3c811504fc30030334f81ddfbee3cfdd1ea7ea
Gabriel Santos <gabmaxsan@gmail.com>
2018-02-04 17:21:49 -0200

    add-methods-restaurantes

--- src/pages/restaurantes/restaurantes.ts
+++ src/pages/restaurantes/restaurantes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Restaurante } from '../../model/restaurante/restaurante';
+import { Http } from '@angular/http';
+import { LoadedModule } from 'ionic-angular/util/module-loader';
 
 @Component({
   selector: 'page-restaurantes',
@@ -8,7 +10,53 @@ import { Restaurante } from '../../model/restaurante/restaurante';
 })
 export class RestaurantesPage {
 
-  constructor(public navCtrl: NavController) {
-  }
+  public restaurantes: Restaurante[];
+  private loader: any;
+
+  constructor(
+    public navCtrl: NavController,
+    private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController
+  ) {}
   
+  ngOnInit(){
+    this.createLoader();
+    this.createHttpRequest();
+  }
+
+  private createLoader() {
+    this.loader = this._loadingCtrl.create({
+      content: 'Listando restaurantes. Aguarde...'
+    });
+    this.loader.present();
+  }
+
+  private createHttpRequest() {
+    this._http
+    .get('http://marmita.idsgeo.com/index.php/page/get_ionic')
+    .map(res => res.json())
+    .toPromise()
+    .then(restaurantes => {
+      this.restaurantes = restaurantes;
+      this.loader.dismiss();
+    })
+    .catch(err => {
+      console.log(err);
+      this.createAlert();
+    });
+  }
+
+  private createAlert() {
+    this._alertCtrl.create({
+      title: 'Falha na conexão',
+      buttons: [{ text: 'Estou ciente!' }],
+      subTitle: 'Não foi possível obter a lista de restaurantes. Tente Novamente.'
+    }).present();
+  }
+
+  getRestaurante(restaurante) {
+    console.log('Entrou na Action getRestaurante');
+  }
+
 }
